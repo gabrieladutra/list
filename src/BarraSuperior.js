775a8e4bfe823eeeec045010eae4265e2b88b930
gabrieladutra <gabriela_ddutra@outlook.com>
2020-03-19 11:50:23 -0300

    Criação do front end

---
+++ src/BarraSuperior.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
+function BarraSuperior(){
+    return (
+     <AppBar position="relative">
+         <p>Lista de Compras</p>
+
+     </AppBar>   
+    )
+}
+export default BarraSuperior
